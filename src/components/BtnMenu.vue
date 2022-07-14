8456af70afe5aecee9fa37156dcf1f714b220231
GuyFox <artm.orlov.88@list.ru>
2022-07-14 22:40:07 +0500

    reactor: change style for drop items of button menu

--- src/components/BtnMenu.vue
+++ src/components/BtnMenu.vue
@@ -3,12 +3,12 @@
     <div class="btnMenu__text">Menu</div>
     <ul class="btnMenu__items">
       <li class="btnMenu__item speechPart">
-        <span>speech part</span>
-        <!-- <ul class="btnMenu__subitems">
+        <span class="item__speechPart">speech part</span>
+        <ul class="item__subitems">
           <li>nouns</li>
           <li>adjectives</li>
           <li>verbs</li> 
-        </ul> -->
+        </ul>
       </li>
       <li class="btnMenu__item">Example</li>
       <li class="btnMenu__item">Example</li>
@@ -39,7 +39,19 @@
   }
 
   .btnMenu:hover .btnMenu__items {
-    display: block;
+    /* display: block; */
+    width: 200px;
+    height: auto;
+    overflow: inherit;
+
+    margin: 10px 0 0 0;
+    padding: 10px 15px;
+    border: 1px solid #e9edc9;
+    border-radius: 5px;
+    box-shadow: 0px 1px 7px #e9edc9;
+    opacity: 1;
+
+    transition: opacity .1s linear;
   }
 
   .top__btnMenu:hover .btnMenu__text {
@@ -59,18 +71,11 @@
   }
 
   .btnMenu__items {
-    display: none;
-    width: 200px;
-
-    margin: 10px 0 0 0;
-    padding: 7px 15px;
-    border: 1px solid #e9edc9;
-    border-radius: 5px;
-    box-shadow: 0px 1px 7px #e9edc9;
-  }
-
-  .speechPart {
-    display: flex;
+    /* display: none; */
+    width: 0px;
+    height: 0px;
+    overflow: hidden;
+    opacity: 0;
   }
 
   .btnMenu__item {
@@ -86,5 +91,44 @@
   .btnMenu__item:hover {
     box-shadow: 0px 1px 7px #e9edc9;
     transition: box-shadow .1s linear;
+  }
+
+  .speechPart {
+    position: relative;
+    display: flex;
+  }
+
+  .item__speechPart {
+    display: block;
+    flex-grow: 1;
+  }
+
+  .item__subitems {
+    position: absolute;
+    top: 0;
+    right: -170px;
+
+    width: 150px;
+
+    padding: 7px 15px;
+    border: 1px solid #e9edc9;
+    border-radius: 5px;
+    box-shadow: 0px 1px 7px #e9edc9;
+    z-index: 10;
+  }
+
+  .item__subitems > li {
+    width: 100%;
+    height: 25px;
+    padding: 0 10px;
+  }
+
+  .item__subitems > li:hover {
+    box-shadow: 0px 1px 7px #e9edc9;
+    transition: box-shadow .1s linear;
+  }
+
+  .item__subitems > li:not(:last-child) {
+    margin-bottom: 7px;
   }
 </style>
