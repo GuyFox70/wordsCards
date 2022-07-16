66aebc9f83e25c4343100302590bb09f366a16bf
GuyFox <artm.orlov.88@list.ru>
2022-07-17 00:12:46 +0500

    feat: add subitems for item of main menu

--- src/components/BtnMenu.vue
+++ src/components/BtnMenu.vue
@@ -1,22 +1,40 @@
 <template>
-  <div class="top__btnMenu btnMenu">
-    <div class="btnMenu__text">Menu</div>
-    <ul class="btnMenu__items">
-      <li class="btnMenu__item speechPart">
-        <span class="item__speechPart">speech part</span>
-        <ul class="item__subitems">
-          <li>nouns</li>
-          <li>adjectives</li>
-          <li>verbs</li> 
-        </ul>
-      </li>
-      <li class="btnMenu__item">Example</li>
-      <li class="btnMenu__item">Example</li>
-      <li class="btnMenu__item">Example</li>
-      <li class="btnMenu__item">Example</li>
-      <li class="btnMenu__item">Example</li>
-      <li class="btnMenu__item">Example</li>
-    </ul>
+  <div class="top__btnMenu menu">
+    <div class="menu__name">Menu</div>
+
+    <div class="menu__wrap-items">
+      <ul class="menu__items">
+
+        <li class="menu__item border showSubItems">
+          <div class="submenu__itemName speechPart">speech part</div>
+  
+          <ul class="submenu__items">
+            <li class="submenu__item">nouns</li>
+            <li class="submenu__item">adjectives</li>
+            <li class="submenu__item">verbs</li> 
+          </ul>
+        </li>
+
+        <li class="menu__item border">
+          <div class="submenu__itemName">example</div>
+        </li>
+        <li class="menu__item border">
+          <div class="submenu__itemName">example</div>
+        </li>
+        <li class="menu__item border">
+          <div class="submenu__itemName">example</div>
+        </li>
+        <li class="menu__item border">
+          <div class="submenu__itemName">example</div>
+        </li>
+        <li class="menu__item ">
+          <div class="submenu__itemName">example</div>
+        </li>
+        <li class="menu__item border">
+          <div class="submenu__itemName">example</div>
+        </li>
+      </ul>
+    </div>
   </div> 
 </template>
 <script>
@@ -33,102 +51,114 @@
   }
 </script>
 <style scoped>
-  .btnMenu {
+  .menu {
     width: 70px;
     height: 50px;
   }
 
-  .btnMenu:hover .btnMenu__items {
-    /* display: block; */
-    width: 200px;
-    height: auto;
-    overflow: inherit;
-
-    margin: 10px 0 0 0;
-    padding: 10px 15px;
-    border: 1px solid #e9edc9;
-    border-radius: 5px;
-    box-shadow: 0px 1px 7px #e9edc9;
-    opacity: 1;
-
-    transition: opacity .1s linear;
-  }
-
-  .top__btnMenu:hover .btnMenu__text {
+  .top__btnMenu:hover .menu__name {
     border: 1px solid #d4a373;
   }
 
-  .btnMenu__text {
+  .menu:hover .menu__wrap-items {
+    transform: scale(1);
+  }
+
+  .menu__name {
+    height: 36px;
     cursor: pointer;
     border: 1px solid transparent;
-    width: 100%;
-    height: 35px;
-    margin: 8px 0 0 0;
+    margin: 7px 0;
 
     display: flex;
     align-items: center;
     justify-content: center;
   }
 
-  .btnMenu__items {
-    /* display: none; */
-    width: 0px;
-    height: 0px;
-    overflow: hidden;
-    opacity: 0;
+  .menu__wrap-items {
+    width: 170px;
+    transform: scale(0);
+    border: 1px solid transparent;
   }
 
-  .btnMenu__item {
-    cursor: pointer;
-    height: 25px;
-    padding: 0 10px;
-  }
-
-  .btnMenu__item:not(:last-child) {
-    margin-bottom: 7px;
-  }
-
-  .btnMenu__item:hover {
-    box-shadow: 0px 1px 7px #e9edc9;
-    transition: box-shadow .1s linear;
-  }
-
-  .speechPart {
-    position: relative;
-    display: flex;
-  }
-
-  .item__speechPart {
-    display: block;
-    flex-grow: 1;
-  }
-
-  .item__subitems {
-    position: absolute;
-    top: 0;
-    right: -170px;
-
-    width: 150px;
-
-    padding: 7px 15px;
+  .menu__items {
+    margin: 2px 0 0 0;
     border: 1px solid #e9edc9;
     border-radius: 5px;
     box-shadow: 0px 1px 7px #e9edc9;
-    z-index: 10;
+    padding: 8px;
   }
 
-  .item__subitems > li {
-    width: 100%;
-    height: 25px;
-    padding: 0 10px;
+  .menu__item {
+    height: 30px;
+    border-radius: 5px;
+    cursor: pointer;
+
+    position: relative;
+    z-index: 0;
   }
 
-  .item__subitems > li:hover {
+  .menu__item:hover {
+    width: 315px;
+  }
+
+  .menu__item:not(:last-child) {
+    margin-bottom: 5px;
+  }
+
+  .submenu__itemName {
+    width: 150px;
+    height: 100%;
+    line-height: 25px;
+    padding: 0 5px;
+
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 5;
+  }
+
+  .speechPart:after {
+    content: '\27A4';
+    margin: 3px 0 0 40px;
+    z-index: 1;
+  }
+
+  .border:hover .submenu__itemName {
     box-shadow: 0px 1px 7px #e9edc9;
-    transition: box-shadow .1s linear;
   }
 
-  .item__subitems > li:not(:last-child) {
-    margin-bottom: 7px;
+  .showSubItems:hover .submenu__items {
+    transform: scale(1);
+  }
+
+  .submenu__items {
+    width: 150px;
+    border: 1px solid #e9edc9;
+    border-radius: 5px;
+    box-shadow: 0px 1px 7px #e9edc9;
+    padding: 7px;
+    transform: scale(0);
+
+    position: absolute;
+    top: 0;
+    right: 0;
+    z-index: 5;
+  }
+
+  .submenu__item {
+    width: 100%;
+    height: 30px;
+
+    line-height: 25px;
+    padding: 0 5px;
+  }
+
+  .submenu__item:not(:last-child) {
+    margin-bottom: 5px;
+  }
+
+  .submenu__item:hover {
+    box-shadow: 0px 1px 7px #e9edc9;
   }
 </style>
