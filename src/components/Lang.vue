04ea848c6041bd8107c178c06f8e0ede65e57cd3
GuyFox <artm.orlov.88@list.ru>
2022-08-28 16:06:24 +0500

    refactor: change style for btn lang

--- src/components/Lang.vue
+++ src/components/Lang.vue
@@ -1,16 +1,15 @@
 <template>
-  <div class="top__switchLang switchLang">
-    <div class="switchLang__nameItem">Lang</div>
-    <div class="switchLang__wrap-options">
-      <div class="switchLang__options">
-        <div class="switchLang__optionsItem">
-          <input type="radio" name="lang" id="eng" checked/>
-          <label for="eng">English</label>
-        </div>
-        <div class="switchLang__optionsItem">
-          <input type="radio" name="lang" id="ru"/>
-          <label for="ru">Russian</label>
-        </div>
+  <div class="top__lang lang">
+    <div class="lang__btn">Lang</div>
+    
+    <div class="lang__list">
+      <div class="lang__item">
+        <input type="radio" name="lang" id="eng" checked/>
+        <label for="eng">English</label>
+      </div>
+      <div class="lang__item">
+        <input type="radio" name="lang" id="ru"/>
+        <label for="ru">Russian</label>
       </div>
     </div>
   </div>
@@ -29,82 +28,5 @@
   }
 </script>
 <style scoped>
-  .switchLang {
-    width: 70px;
-    height: 50px;
-  }
-
-  .switchLang__nameItem {
-    height: 36px;
-    cursor: pointer;
-    border: 1px solid transparent;
-    margin: 7px 0;
-
-    display: flex;
-    align-items: center;
-    justify-content: center;  
-  }
-
-  .top__switchLang:hover .switchLang__nameItem {
-    border: 1px solid var(--white);
-  }
-
-  .switchLang__wrap-options {
-    width: 120px;
-    transform: scale(0);
-    border: 1px solid transparent;
-    position: relative;
-    z-index: 1;
-  }
-
-  .switchLang__options {
-    margin: 2px 0 0 0;
-    border: 1px solid var(--bg-color);
-    border-radius: 5px;
-    box-shadow: 0px 1px 7px var(--shadow-dark);
-    padding: 8px;
-  }
-
-  .switchLang:hover .switchLang__wrap-options {
-    transform: scale(1);
-  }
-
-  .switchLang__optionsItem {
-    border-radius: 5px;
-    height: 30px;
-
-    display: flex;
-    align-items: center;
-  }
-
-  .switchLang__optionsItem:hover {
-    box-shadow: 0px 1px 7px rgba(0,0,0,.3);
-    transition: box-shadow .1s linear;
-  }
-
-  .switchLang__optionsItem > input {
-    margin: 0 7px 0 7px;
-    cursor: pointer;
-    display: none;
-  }
-
-  .switchLang__optionsItem > label {
-    cursor: pointer;
-    line-height: 20px;
-    display: flex;
-    align-items: center;
-  }
-
-  .switchLang__optionsItem > label:before {
-    display: block;
-    content: "";
-    width: 17px;
-    height: 17px;
-    background: url("../assets/img/radio.svg");
-    margin: 0 7px 0 7px;
-  }
-
-  .switchLang__optionsItem > input:checked + label:before {
-    background: url("../assets/img/radioChecked.svg");
-  }
+  @import url('../assets/css/btnLang.css');
 </style>
